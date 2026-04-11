7f36d3d1fa099f32e348c5b51305260bf85ca41a
mamitasse <mamantasse@gmail.com>
2026-04-11 22:50:49 +0200

    affichage panier suite bouton ajoute et suprimer et valider panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,17 +2,62 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class CartController extends AbstractController
+class CartController extends AbstractController
 {
-    #[Route('/cart', name: 'app_cart')]
-    public function index(): Response
+    #[Route('/cart/add/{id}', name: 'app_cart_add')]
+    public function add(int $id, SessionInterface $session): Response
     {
+        $cart = $session->get('cart', []);
+
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/cart', name: 'app_cart')]
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
+    {
+        $cart = $session->get('cart', []);
+
+        $cartData = [];
+        $total = 0;
+
+        foreach ($cart as $id => $quantity) {
+            $product = $productRepository->find($id);
+
+            if ($product) {
+                $cartData[] = [
+                    'product' => $product,
+                    'quantity' => $quantity,
+                ];
+
+                $total += $product->getPrice() * $quantity;
+            }
+        }
+
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            'cart' => $cartData,
+            'total' => $total,
         ]);
     }
+
+    #[Route('/cart/clear', name: 'app_cart_clear')]
+public function clear(SessionInterface $session): Response
+{
+    $session->remove('cart');
+
+    return $this->redirectToRoute('app_cart');
+}
 }
